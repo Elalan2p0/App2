005e3e0d4371f1ecefe5747e9db87099f42ec05c
JORGE ALAN SALAS MONTOYA <2018371023@uteq.edu.mx>
2021-10-05 19:56:59 -0500

    Agregar archivos de proyecto.

---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App2.ViewModels;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainPageViewModel();
+        }
+    }
+}
---
+++ App2/App2/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace App2.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+    
+        private string _nombre;
+        public MainPageViewModel() 
+        {
+            _nombre = "holis";
+        }
+
+        public string Nombre 
+        { 
+            get => _nombre;
+            set 
+            {
+
+                if (string.Equals(_nombre, value)) return;
+
+                _nombre = value;
+                RaisePropertyChanged(nameof(Nombre));
+                RaisePropertyChanged(nameof(SinUpdate));
+            }
+        }
+
+        public string SinUpdate 
+        { 
+            get => _nombre;
+        }
+
+    }
+}
